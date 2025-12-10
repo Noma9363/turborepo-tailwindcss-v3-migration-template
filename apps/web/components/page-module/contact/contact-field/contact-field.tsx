132a2adfea476773d4a611ec6399f8909fd46e02
noma9363 <noma9363@gmail.com>
2025-12-10 12:26:30 +0900

    feature: contact page updage
    from contact.page when input or textarea are filled
    
    send into firebase storage successfully

--- apps/web/components/page-module/contact/contact-field/contact-field.tsx
+++ apps/web/components/page-module/contact/contact-field/contact-field.tsx
@@ -7,42 +7,88 @@ import {Textarea} from "@workspace/ui/components/ui/textarea";
 import {Button} from "@workspace/ui/components/ui/button";
 import {typography} from "@workspace/ui/components/ui/tailwind-variations";
 
+interface ContactFieldProps{
+    loading: boolean;
+    formData: {
+        name: string;
+        email: string;
+        message: string;
+    };
+    onSubmit: (e: React.FormEvent) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    status: 'idle' | 'success' | 'error';
+}
+
 export const ContactField = (
     {
-        className,
-        ...props
-    }:React.ComponentProps<"div">
+        loading,
+        formData,
+        onSubmit,
+        onChange,
+        status,
+
+    }:ContactFieldProps
 ) => {
     return (
-        <div className={cn("", className)} {...props}>
+        <div className={cn("page-container")} >
             <Card variant="glass" className="max-w-md mx-auto">
                 <CardHeader className="pb-4">
                     <CardDescription className={cn("w-fit ", typography.inlineCodeTypo)}>Contact field, Enter your email.</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form className="flex flex-col justify-between gap-6">
+                    <form
+                        className="flex flex-col justify-between gap-6"
+                        onSubmit={onSubmit}
+                    >
+                        <Field>
+                            <FieldLabel htmlFor="name">Name</FieldLabel>
+                            <Input
+                                className=""
+                                id="name"
+                                name="name"
+                                type="text"
+                                placeholder="example last name"
+                                value={formData.name}
+                                onChange={onChange}
+                                disabled={loading}
+                                required
+                            />
+                        </Field>
                         <Field>
                             <FieldLabel htmlFor="email">Email</FieldLabel>
                             <Input
                                 className=""
                                 id="email"
-                                typeof="email"
+                                name="email"
+                                type="email"
                                 placeholder="m@example.com"
+                                value={formData.email}
+                                onChange={onChange}
+                                disabled={loading}
                                 required
                             />
                         </Field>
                         <Field>
-                            <FieldLabel htmlFor="comments">
+                            <FieldLabel htmlFor="message">
                                 Comments
                             </FieldLabel>
                             <Textarea
-                                id="comments"
+                                id="message"
+                                name="message"
                                 placeholder="Add any comments"
                                 className={cn("resize-none")}
+                                value={formData.message}
+                                onChange={onChange}
+                                disabled={loading}
+                                required
                             />
                         </Field>
                         <Field>
-                            <Button typeof="submit">Submit.</Button>
+                            <Button type="submit" disabled={loading}>
+                                {
+                                    loading ? "Sending..." : "Submit."
+                                }
+                            </Button>
                         </Field>
                     </form>
                 </CardContent>
--- apps/web/components/page/contact.tsx
+++ apps/web/components/page/contact.tsx
@@ -1,11 +1,57 @@
 'use client'
 
 import React from "react";
+import {collection, addDoc} from 'firebase/firestore';
+import {db} from "@/lib/firebase";
 import {cn} from "@workspace/ui/lib/utils";
 import {Headline} from "@/components/common/headline/headline";
 import {ContactField} from "@/components/page-module/contact/contact-field/contact-field";
 
 export const ContactPage = () => {
+    const [formData, setFormData] = React.useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    // used in sending
+    const [loading, setLoading] = React.useState(false);
+    // form status
+    const [statues, setStatues] = React.useState<'idle' | 'success' | 'error'>('idle');
+
+    // handler
+    const handleSubmit = async (e: React.FormEvent)=>{
+        e.preventDefault(); // block refreshing
+        setLoading(true); // loading phase
+        setStatues('idle'); // set Form Status
+
+        try{
+            await addDoc(collection(db, 'contacts'), {
+                ...formData,
+                timestamp: new Date(),
+            });
+
+            // successes case
+            setStatues('success');
+            setFormData({name: '', email: '', message: ''});
+        } catch (error){
+            // case: error
+            console.error('Error submitting: ', error);
+            setStatues('error');
+        } finally {
+            setLoading(false); // finish phase
+        }
+    }
+
+    const handleChange = (
+        e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
 
     return(
         <div id="contact" className={cn("w-full pb-16")}>
@@ -14,7 +60,13 @@ export const ContactPage = () => {
                 <Headline level="h2" className="text-center">Contact.</Headline>
                 <Headline level="lead" className="text-center break-all">관심이 있으시다면...</Headline>
             </div>
-            <ContactField className="page-container"/>
+            <ContactField
+                formData={formData}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                loading={loading}
+                status={statues}
+            />
         </div>
     )
 }
