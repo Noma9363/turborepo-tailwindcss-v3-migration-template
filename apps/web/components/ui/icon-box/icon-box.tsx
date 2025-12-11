5707c4776d8b60db31fd558c3af412cfc608b11c
noma9363 <noma9363@gmail.com>
2025-12-11 12:33:29 +0900

    refix

--- apps/web/components/ui/icon-box/icon-box.tsx
+++ apps/web/components/ui/icon-box/icon-box.tsx
@@ -8,7 +8,7 @@ import CSSMetaBalls, {CSSMetaBallsProps} from "@workspace/ui/components/reactbit
 
 type BoxSize = {
     sm: 'w-12 h-12';
-    md: 'w-16 h-16';
+    md: 'w-20 h-20';
     lg: 'w-20 h-20';
     xl: 'w-24 h-24';
     "2xl": 'w-32 h-32';
