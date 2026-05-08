8bd29a0233fe4a37401ce5f877b109bc776f04cb
Noma9363 <noma9363@gmail.com>
2026-05-08 11:41:07 +0900

    fix: '{}' error in vercel

--- apps/web/components/page/about.tsx
+++ apps/web/components/page/about.tsx
@@ -43,7 +43,6 @@ export const About = () => {
             <div className="pb-12"></div>
         </section>
         <HorizontalScroll/>
-        {/* section-3 */}
         <section id="about" className={cn("page-container w-full flex flex-col items-center justify-stretch")}>
             <div
                 className={cn(" " + "w-full flex flex-col align-middle items-center justify-start" + " h-full gap-2")}
